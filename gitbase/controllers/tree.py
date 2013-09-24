947b9575b298e422897e8a5cc48d63319706d0b9
Mike Boers <github@mikeboers.com>
2013-09-24 10:15:57 -0700

    Tree at different commits

--- gitbase/controllers/tree.py
+++ gitbase/controllers/tree.py
@@ -3,15 +3,18 @@ import pygit2
 from . import *
 
 
-@app.route('/<repo:repo>/tree/master/<path:path>')
-def tree(repo, path):
+@app.route('/<repo:repo>/tree/HEAD/<path:path>')
+@app.route('/<repo:repo>/tree/<ref>/<path:path>')
+def tree(repo, path, ref='HEAD'):
 
-    entry = repo.git.head.get_object().tree[path]
+    commit = repo.git.revparse_single(ref)
+    entry = commit.tree[path]
     obj = repo.git[entry.oid]
+
     if isinstance(obj, pygit2.Tree):
-        return render_template('tree.haml', repo=repo, path=path, tree=obj)
+        return render_template('tree.haml', repo=repo, commit=commit, path=path, tree=obj)
     elif isinstance(obj, pygit2.Blob):
-        return render_template('blob.haml', repo=repo, path=path, blob=obj)
+        return render_template('blob.haml', repo=repo, commit=commit, path=path, blob=obj)
 
     abort(404)
 
