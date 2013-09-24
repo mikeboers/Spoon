8371bbd4ade2f45431163085c336e9ec2aaf218f
Mike Boers <github@mikeboers.com>
2013-09-23 19:06:34 -0700

    Inprove tree view

--- gitbase/controllers/tree.py
+++ gitbase/controllers/tree.py
@@ -3,7 +3,7 @@ import pygit2
 from . import *
 
 
-@app.route('/<repo:repo>/<path:path>')
+@app.route('/<repo:repo>/master/tree/<path:path>')
 def tree(repo, path):
 
     entry = repo.git.head.get_object().tree[path]
