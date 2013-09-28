7be778a3371cc967ee9a281ad77e9b14440480d1
Mike Boers <github@mikeboers.com>
2013-09-27 20:36:44 -0700

    Split repo page into two

--- gitbase/controllers/repo.py
+++ gitbase/controllers/repo.py
@@ -4,3 +4,8 @@ from . import *
 @app.route('/<repo:repo>')
 def repo(repo):
     return render_template('repo.haml', repo=repo, group=repo.group)
+
+
+@app.route('/<repo:repo>/commits')
+def commits(repo):
+    return render_template('commits.haml', repo=repo)
