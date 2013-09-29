c8ddcda5df4afca87a844b91a71cd138c2e1c0d0
Mike Boers <github@mikeboers.com>
2013-09-29 09:01:22 -0700

    Move repo walk logic into controller

--- etc/flask/500-gitbase.py
+++ etc/flask/500-gitbase.py
@@ -6,3 +6,5 @@ GROUP_NAME_RE = r'[\w\.-]+'
 REPO_NAME_RE  = r'[\w\.-]+'
 
 SSH_KEYS_PATH = os.path.join(ROOT_PATH, 'var', 'authorized_keys')
+
+COMMITS_PER_PAGE = 50
--- gitbase/controllers/repo.py
+++ gitbase/controllers/repo.py
@@ -1,3 +1,6 @@
+import itertools
+import pygit2 as git
+
 from . import *
 
 
@@ -8,4 +11,45 @@ def repo(repo):
 
 @app.route('/<repo:repo>/commits')
 def commits(repo):
-    return render_template('commits.haml', repo=repo)
+
+    if 'after' in request.args:
+        try:
+            head = repo.git.revparse_single(request.args['after'])
+        except git.GitError:
+            abort(404)
+    else:
+        try:
+            head = repo.git.head.get_object()
+        except git.GitError:
+            head = None
+
+    if not head:
+        return render_template('commits.haml', repo=repo)
+
+    # Getting the set of commits and the next one are easy. Getting the
+    # start of the previous set? Not so much...
+    walker = repo.git.walk(head.oid, git.GIT_SORT_TOPOLOGICAL)
+    commits = list(itertools.islice(walker, app.config['COMMITS_PER_PAGE']))
+    next_commit = next(walker, None)
+
+    # We need to start at the HEAD and walk until we find it.
+    prev_commits = []
+    for commit in repo.git.walk(repo.git.head.get_object().oid, git.GIT_SORT_TOPOLOGICAL):
+        if commit.oid == head.oid:
+            break
+        prev_commits.append(commit)
+    prev_commits = prev_commits[-50:]
+    prev_commit = prev_commits[0] if prev_commits else None
+
+    hex_to_refs = {}
+    for ref in repo.git.listall_references():
+        hex_to_refs.setdefault(repo.git.revparse_single(ref).oid, []).append(ref)
+
+    return render_template('commits.haml',
+        repo=repo,
+        head=head,
+        commits=commits,
+        next_commit=next_commit,
+        prev_commit=prev_commit,
+        hex_to_refs=hex_to_refs,
+    )
