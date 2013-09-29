b7e7e73b73209b395ee065d3ccdb5100ec40df4a
Mike Boers <github@mikeboers.com>
2013-09-28 21:32:26 -0700

    Refining the repo pages

--- gitbase/controllers/tree.py
+++ gitbase/controllers/tree.py
@@ -5,18 +5,20 @@ import pygit2
 from . import *
 
 
+@app.route('/<repo:repo>/tree/HEAD')
+@app.route('/<repo:repo>/tree/<ref>')
 @app.route('/<repo:repo>/tree/HEAD/<path:path>')
 @app.route('/<repo:repo>/tree/<ref>/<path:path>')
-def tree(repo, path, ref='HEAD'):
+def tree(repo, path='', ref='HEAD'):
 
     commit = repo.git.revparse_single(ref)
-    entry = commit.tree[path]
+    entry = commit.tree[path] if path else commit.tree
     obj = repo.git[entry.oid]
 
     if isinstance(obj, pygit2.Tree):
-        return render_template('tree.haml', repo=repo, commit=commit, path=path, tree=obj)
+        return render_template('tree.haml', repo=repo, ref=ref, commit=commit, path=path, tree=obj)
     elif isinstance(obj, pygit2.Blob):
-        return render_template('blob.haml', repo=repo, commit=commit, path=path, blob=obj)
+        return render_template('blob.haml', repo=repo, ref=ref, commit=commit, path=path, blob=obj)
 
     abort(404)
 
