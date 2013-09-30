aa650d68c4940606117b733dee795956bebb5d9b
Mike Boers <github@mikeboers.com>
2013-09-30 13:54:02 -0700

    Use //= require_self

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-
-//= require _init
+//= require_self
 //= require pages/cpanel_users
 //= require pages/repo_admin
 //= require pages/group_admin
+
+
+var app = {};
+app._endpoint_handlers = {}
+
+app.endpoint_handler = function(endpoint, func) {
+    app._endpoint_handlers[endpoint] = app._endpoint_handlers[endpoint] || []
+    app._endpoint_handlers[endpoint].push(func)
+}
+
+jQuery(function($) {
+
+    app.endpoint = $('body').data('endpoint');
+    var endpoint_handlers = app._endpoint_handlers[app.endpoint] || [];
+    $.each(endpoint_handlers, function(i, func) {
+        func();
+    });
+    
+});
 
