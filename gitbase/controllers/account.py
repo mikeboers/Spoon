c6d8903cf08ead67b4dd239515df928f69a54adb
Mike Boers <github@mikeboers.com>
2013-10-02 16:29:51 -0700

    Account meta data, gravatars, and style tweaks

--- gitbase/controllers/account.py
+++ gitbase/controllers/account.py
@@ -10,11 +10,25 @@ class NewRepoForm(Form):
     public = wtf.fields.BooleanField(default=True)
 
 
+class AccountMetaForm(Form):
+
+    email = wtf.fields.TextField(validators=[wtf.validators.email()])
+    url = wtf.fields.TextField(validators=[wtf.validators.url()])
+    description = wtf.fields.TextAreaField()
+
+
 @app.route('/<account:account>', methods=['GET', 'POST'])
 def account(account):
 
     new_repo_form = NewRepoForm()
+    account_meta_form = AccountMetaForm(obj=account)
 
+    if request.method == 'POST' and request.form.get('action') == 'account.meta.write':
+        auth.assert_can('account.write', account)
+        account_meta_form.populate_obj(account)
+        flash('Updated account info.')
+        db.session.commit()
+        
     if request.method == 'POST' and request.form.get('action') == 'account.delete':
         auth.assert_can('account.delete', account)
         if request.form.get('user_accepted_danger'):
@@ -24,4 +38,8 @@ def account(account):
         else:
             flash('Javascript is required to delete groups.', 'danger')
 
-    return render_template('account/account.haml', account=account, new_repo_form=new_repo_form)
+    return render_template('account/account.haml',
+        account=account,
+        new_repo_form=new_repo_form,
+        account_meta_form=account_meta_form,
+    )
