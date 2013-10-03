6a415f0c0be752bd1a7677fd45d5738a1caf2ece
Mike Boers <github@mikeboers.com>
2013-10-03 12:50:03 -0700

    Sort accounts by name

--- gitbase/controllers/index.py
+++ gitbase/controllers/index.py
@@ -5,5 +5,5 @@ def index():
 
     groups = Account.query.options(sa.orm.joinedload('repos')).all()
     groups = [g for g in groups if auth.can('account.read', g)]
-    
+    groups.sort(key=lambda g: g.name)
     return render_template('index.haml', groups=groups)
