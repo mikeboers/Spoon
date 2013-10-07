a749af29d4ab3af6de76663d1d7a2a6cd7db1e58
Mike Boers <github@mikeboers.com>
2013-10-07 15:00:52 -0700

    Reflow the index

--- spoon/controllers/index.py
+++ spoon/controllers/index.py
@@ -2,8 +2,7 @@ from . import *
 
 @app.route('/')
 def index():
-
-    groups = Account.query.options(sa.orm.joinedload('repos')).all()
-    groups = [g for g in groups if auth.can('account.read', g)]
-    groups.sort(key=lambda g: g.name)
-    return render_template('index.haml', groups=groups)
+    accounts = Account.query.options(sa.orm.joinedload('repos')).all()
+    accounts = [g for g in accounts if auth.can('account.read', g)]
+    accounts.sort(key=lambda g: g.name)
+    return render_template('index.haml', accounts=accounts)
