c055cbcf6d7143c1b4aa011de9a71c519d4437d8
Mike Boers <github@mikeboers.com>
2013-10-19 16:29:08 -0700

    Persistant SECRET_KEY

--- lib/Flask-Roots/etc/flask/000-master.py
+++ lib/Flask-Roots/etc/flask/000-master.py
@@ -4,11 +4,18 @@ UPLOAD_FOLDER = os.path.join(INSTANCE_PATH, 'uploads')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(INSTANCE_PATH, 'sqlite', 'main.sqlite')
 
-# This will get overriden in a subsequent configfile, but we want something
-# completly random in here just in case.
-SECRET_KEY = os.urandom(32)
+# Create a persistant secret key.
+secret_key_path = os.path.join(INSTANCE_PATH, 'etc', 'secret_key')
+if not os.path.exists(secret_key_path):
+    with open(secret_key_path, 'w') as fh:
+        fh.write(os.urandom(32))
+with open(secret_key_path) as fh:
+    SECRET_KEY = fh.read()
 
+
+# TODO: migrate this to Flask-Imgsizer
 setdefault('MAKO_IMPORTS', []).append('from flask.ext.imgsizer import resized_img_src')
+
 MAKO_MODULE_DIRECTORY = os.path.join(INSTANCE_PATH, 'mako')
 
 setdefault('IMGSIZER_PATH', [])
