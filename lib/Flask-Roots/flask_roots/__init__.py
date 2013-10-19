64178d0f0fd85cc143994a46b95d2a25346b938e
Mike Boers <github@mikeboers.com>
2013-10-19 15:32:11 -0700

    Refactor Roots as extension

--- lib/Flask-Roots/flask_roots/__init__.py
+++ lib/Flask-Roots/flask_roots/__init__.py
@@ -1,2 +1,8 @@
-from .core import make_app
+from .flask import Flask
+from .extension import Roots
+
+def make_app(*args, **kwargs):
+    app = Flask(*args, **kwargs)
+    roots = Roots(app)
+    return roots.export()
 
---
+++ lib/Flask-Roots/flask_roots/extension.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+
+import os
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.imgsizer import ImgSizer
+# from flask.ext.mail import Mail
+from flask.ext.login import LoginManager
+from flask.ext.acl import AuthManager
+
+
+class Roots(object):
+
+    def __init__(self, app):
+        self.init_app(app)
+
+    def init_app(self, app):
+
+        # Link them together.
+        self.app = app
+        app.roots = self
+        app.extensions['roots'] = self
+
+        from .config import make_config
+        app.config.update(make_config(app))
+        app.root_path = app.config['ROOT_PATH']
+
+        from .logs import setup_logs
+        setup_logs(app)
+
+        from .session import setup_session
+        setup_session(app)
+
+        self.login_manager = LoginManager(app)
+        self.auth = AuthManager(app)
+
+        from .mako import MakoTemplates
+        self.mako = MakoTemplates(app)
+
+        self.imgsizer = ImgSizer(app)
+
+        self.db = SQLAlchemy(app)
+        self.db.metadata.bind = self.db.engine # WTF do I need to do this for?!
+
+        from .routing import setup_routing
+        setup_routing(app)
+
+        from .errors import setup_errors
+        setup_errors(app)
+
+    def export(self):
+        return dict(
+            app=self.app,
+            mako=self.mako,
+            auth=self.auth,
+            login_manager=self.login_manager,
+            imgsizer=self.imgsizer,
+            db=self.db,
+        )
+
